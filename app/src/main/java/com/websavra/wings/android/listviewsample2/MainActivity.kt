94b77862a5ea831d2b78ab6aad17c097d6c3cca0
Ryosuke Ito <Ryosuke Ito>
2020-02-02 16:03:06 +0900

    feat: Activity edit

--- app/src/main/java/com/websavra/wings/android/listviewsample2/MainActivity.kt
+++ app/src/main/java/com/websavra/wings/android/listviewsample2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.websavra.wings.android.listviewsample2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -15,5 +17,14 @@ class MainActivity : AppCompatActivity() {
         val menuList = mutableListOf("から揚げ定食","ハンバーグ定食","生姜焼き定食","ステーキ定食","野菜炒め定食","とんかつ定食","ミンチかつ定食","チキンカツ定食","コロッケ定食","焼き魚定食","焼肉定食")
         val adapter = ArrayAdapter(applicationContext, android.R.layout.simple_list_item_1, menuList)
         lvMenu.adapter = adapter
+
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener: AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val dialogFragment = OrderConfirmDialogFragment()
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+        }
     }
 }
